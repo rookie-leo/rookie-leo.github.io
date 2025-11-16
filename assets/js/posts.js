676099e23ff6603de3fc29a061e44d1b017f3f68
rookie-leo <proleonardopa@gmail.com>
2025-11-16 12:06:24 -0300

    Estrutura inicial do blog

---
+++ assets/js/posts.js
@@ -0,0 +1,9 @@
+const posts = [
+    {
+        id: "exemplo",
+        title: "Meu primeiro artigo",
+        date: "2025-02-20",
+        summary: "Este é um exemplo de artigo escrito em Markdown.",
+        file: "posts/exemplo.md"
+    }
+];
